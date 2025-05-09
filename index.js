c23c0db63bcde0777e03578f6d75e2cc665ce9f4
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-09 18:50:52 +0500

    removed vercel

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
 const message = `🚀 Hello from GitHub Actions! (${new Date().toLocaleString()})`;
 
-axios
-  .post(url, {
-    chat_id: chatId,
-    text: message,
-  })
-  .then((res) => {
-    console.log("✅ Message sent:", res.data);
-  })
-  .catch((err) => {
-    console.error("❌ Error sending message:", err.response?.data || err.message);
-    process.exit(1);
-  });
+axios.post(url, {
+  chat_id: chatId,
+  text: message,
+}).then((res) => {
+  console.log("✅ Message sent:", res.data);
+  process.exit(0); // clean exit
+}).catch((err) => {
+  console.error("❌ Error sending message:", err.response?.data || err.message);
+  process.exit(1);
+});
